0665577ca93f9a15b7f8748de3852857855211e5
SatoTakumi0613 <ts11650755@gmail.com>
2020-11-01 17:01:56 +0900

    feat: add stateless widget

--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -1,44 +1,23 @@
 import 'package:flutter/material.dart';
 
-class UsersPage extends StatefulWidget {
+class UsersPage extends StatelessWidget {
   UsersPage({Key key, this.title}) : super(key: key);
   final String title;
-  @override
-  _UsersPageState createState() => _UsersPageState();
-}
-
-class _UsersPageState extends State<UsersPage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Users page',
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
