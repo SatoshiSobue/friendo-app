181c42640af250629701b5456afd29654c5e66fe
Kotatsun <shine_of_light000@yahoo.co.jp>
2020-12-20 19:44:27 +0900

    add: add user dialog(stateless)

---
+++ lib/widgets/dialog/user_dialog.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:friendo_app/widgets/utils/hex_color.dart';
+import 'package:friendo_app/widgets/utils/icon_widget.dart';
+
+class UserDialog extends StatelessWidget {
+  final String name, pronounce;
+  final DateTime birthday;
+  UserDialog({this.name, this.pronounce, this.birthday});
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Text('show dialog'),
+      onPressed: () async {
+        // show dialog
+        var _ = await showDialog<int>(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              content: Container(
+                height: 390,
+                width: 320,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 56,
+                      width: 56,
+                      child: Container(color: Colors.blue),
+                      margin: EdgeInsets.fromLTRB(97, 0, 0, 0),
+                    ),
+                    SizedBox(height: 17),
+                    Text("Name",
+                        style: TextStyle(fontSize: 12),
+                        textAlign: TextAlign.left),
+                    SizedBox(height: 12.88),
+                    Text(name,
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.left),
+                    Divider(
+                      height: 9.37,
+                      thickness: 2,
+                      color: HexColor("#4A4A4A"),
+                    ),
+                    SizedBox(height: 16),
+                    Text("Name",
+                        style: TextStyle(fontSize: 12),
+                        textAlign: TextAlign.left),
+                    SizedBox(height: 12.88),
+                    Text(pronounce,
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.left),
+                    Divider(
+                      height: 9.37,
+                      thickness: 2,
+                      color: HexColor("#4A4A4A"),
+                    ),
+                    SizedBox(height: 16),
+                    Text("Name",
+                        style: TextStyle(fontSize: 12),
+                        textAlign: TextAlign.left),
+                    SizedBox(height: 12.88),
+                    Text(
+                        '${birthday.year} / ${birthday.month.toString().padLeft(2, "0")} / ${birthday.day}',
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.left),
+                    Divider(
+                      height: 9.37,
+                      thickness: 2,
+                      color: HexColor("#4A4A4A"),
+                    ),
+                    SizedBox(height: 23.45),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                              child: IconWidget(
+                                name: "check_box_gray",
+                                width: 32,
+                                height: 32,
+                              ),
+                            ),
+                            Container(
+                              child: Text('32/35'),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                              child: IconWidget(
+                                name: "memo",
+                                width: 32,
+                                height: 32,
+                              ),
+                            ),
+                            Container(
+                              child: Text('12'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('キャンセル'),
+                  textColor: Colors.black,
+                  onPressed: () => Navigator.of(context).pop(0),
+                ),
+                FlatButton(
+                  child: Text('OK'),
+                  textColor: Colors.black,
+                  onPressed: () => Navigator.of(context).pop(1),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
